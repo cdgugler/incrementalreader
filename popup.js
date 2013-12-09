9b9f1d72921d082588be8257c5cb2bd94321a8db
Cory Gugler <cory@addlime.com>
2013-12-08 20:15:05 -0600

    Remove unused code (popup close notification), moved popup code into iReader namespace, changed some vars to more sane names

--- popup.js
+++ popup.js
@@ -1,53 +1,57 @@
 var iReader = iReader || {};
 
-iReader.popup = {};
-iReader.popup.readLaterBtn = document.querySelector('button.readlater-btn'),
-iReader.popup.articleList = document.querySelector('ul.article-list'),
-iReader.popup.articles = [];
+iReader.readLaterBtn = document.querySelector('button.readlater-btn'),
+iReader.articleUl = document.querySelector('ul.article-list'),
+iReader.articles = [];
+iReader.storage = chrome.storage.local;
 
-var storage = chrome.storage.local;
-
-// notify eventPage when popup closed to clean up list and save
-var background = chrome.extension.getBackgroundPage();
-window.addEventListener("unload", function (event) {
-    background.console.log("Popup closed.");
+iReader.storage.get('articles', function(items) {
+  console.log(items.articles);
+  if (items.articles) {
+      iReader.articles = items.articles;
+      iReader.populateList(iReader.articles);
+  } else {
+      iReader.articles = [];
+  }
 });
 
-function gogogo() {
+iReader.openArticle = function () {
     var id = this.parentNode.dataset.id,
-        location = iReader.popup.articles[id].location,
+        location = iReader.articles[id].location,
         url = this.href;
     // send open request to eventPage
-    chrome.extension.sendMessage({action: "openArticle", location: iReader.popup.articles[id]["location"], url:url }, function(response) {
+    chrome.extension.sendMessage({action: "openArticle", location: iReader.articles[id]["location"], url:url }, function(response) {
         console.log(response);
     });
 }
+
 // updates local storage 
-function addToRead(articles) {
+iReader.saveArticles = function (articles) {
     // Save it using the Chrome extension storage API.
-    storage.set({'articles': articles}, function() {
+    iReader.storage.set({'articles': articles}, function() {
     // Notify that we saved.
     console.log('Saved to Local storage!');
   });
 }
+
 // removes item from popup.html & local storage
-function removeItem() {
+iReader.removeArticle = function () {
     // parent is LI element
     var parent = this.parentNode;
     parent.innerHTML = '';
     var url = parent.dataset.url;
-    for (var i = 0; i < iReader.popup.articles.length; i++) {
-        if (iReader.popup.articles[i].url == url) {
+    for (var i = 0; i < iReader.articles.length; i++) {
+        if (iReader.articles[i].url == url) {
             console.log("Found match!");
-            iReader.popup.articles.splice(i, 1);
+            iReader.articles.splice(i, 1);
             break;
         }
     }
-    addToRead(iReader.popup.articles);
+    iReader.saveArticles(iReader.articles);
     parent.parentNode.removeChild(parent);
 }
 
-function populateList (articles) {
+iReader.populateList = function (articles) {
     for (var i = 0; i < articles.length; i++)
     {
         var newLi = document.createElement("li");
@@ -56,34 +60,23 @@ function populateList (articles) {
         var newText = document.createTextNode(articles[i].title);
         var deleteX = document.createTextNode("X");
 
-        newAnchor.addEventListener('click', gogogo);
-        deleteAnchor.addEventListener('click', removeItem);
+        newAnchor.addEventListener('click', iReader.openArticle);
+        deleteAnchor.addEventListener('click', iReader.removeArticle);
         deleteAnchor.href = "#";
         newLi.setAttribute('data-id', i);
         newLi.setAttribute('data-url', articles[i].url);
         newLi.setAttribute('data-loc', articles[i].location);
         newAnchor.href = articles[i].url;
         deleteAnchor.className = "remove-item";
-
         newAnchor.appendChild(newText);
         deleteAnchor.appendChild(deleteX);
         newLi.appendChild(deleteAnchor);
         newLi.appendChild(newAnchor);
-        iReader.popup.articleList.appendChild(newLi);
+        iReader.articleUl.appendChild(newLi);
     }
 }
 
-storage.get('articles', function(items) {
-  console.log(items.articles);
-  if (items.articles) {
-      iReader.popup.articles = items.articles;
-      populateList(iReader.popup.articles);
-  } else {
-      iReader.popup.articles = [];
-  }
-});
-
-var addPage = function (toReadList) {
+iReader.addArticle = function () {
     var location;
     chrome.tabs.query({
         active: true,
@@ -98,14 +91,9 @@ var addPage = function (toReadList) {
 
         // get the scroll location
         chrome.tabs.sendMessage(id, {action: "reqLoc"}, function (response) {
-            console.log("RESPONSE FROM content.js is: ");
-            console.log(response.location);
-
-            var newToRead = { url: url, title: title, location: response.location }
-            console.log(newToRead);
-            iReader.popup.articles.push(newToRead);
-            addToRead(iReader.popup.articles);
-
+            var article = { url: url, title: title, location: response.location }
+            iReader.articles.push(article);
+            iReader.saveArticles(iReader.articles);
 
             var newItem = document.createElement("li");
             var deleteAnchor = document.createElement("a");
@@ -113,11 +101,11 @@ var addPage = function (toReadList) {
             var newText = document.createTextNode(title);
             var deleteX = document.createTextNode("X");
 
-            newAnchor.addEventListener('click', gogogo);
-            deleteAnchor.addEventListener('click', removeItem);
+            newAnchor.addEventListener('click', iReader.openArticle);
+            deleteAnchor.addEventListener('click', iReader.removeArticle);
             
             deleteAnchor.href = "#";
-            newItem.setAttribute('data-id', iReader.popup.articles.length-1);
+            newItem.setAttribute('data-id', iReader.articles.length-1);
             newAnchor.href = url;
             deleteAnchor.className = "remove-item";
 
@@ -125,7 +113,7 @@ var addPage = function (toReadList) {
             deleteAnchor.appendChild(deleteX);
             newItem.appendChild(deleteAnchor);
             newItem.appendChild(newAnchor);
-            iReader.popup.articleList.appendChild(newItem);
+            iReader.articleUl.appendChild(newItem);
         });
 
     });
@@ -133,4 +121,4 @@ var addPage = function (toReadList) {
 };
 
 
-iReader.popup.readLaterBtn.addEventListener('click', addPage);
+iReader.readLaterBtn.addEventListener('click', iReader.addArticle);
